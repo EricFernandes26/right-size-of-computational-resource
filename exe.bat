38871a42778030fbd31f036a5f95d35b72cec804
Eric Fernandes Pereira <83287307+EricFernandes26@users.noreply.github.com>
2024-03-19 17:15:01 -0300

    Update exe.bat

--- exe.bat
+++ exe.bat
@@ -1,4 +1,4 @@
 @echo off
 
 rem Executa o script Python ec2.py
-"C:\Users\Administrator\AppData\Local\Programs\Python\Python312\python.exe" "C:\Users\Administrator\Desktop\ok\ec2.py"
+"C:\Users\Administrator\AppData\Local\Programs\Python\Python312\python.exe" "C:\Users\Administrator\Desktop\right-size-of-computational-resource\app.py"
